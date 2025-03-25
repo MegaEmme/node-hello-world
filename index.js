851bdd52b7e3b5bbfeb336eda2de917b1cbc065f
MegaEmme <mikmusco@gmail.com>
2025-03-25 14:42:27 +0100

    feat. bonus completed (milestone 5)

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello Boolean");
+// console.log("Hello Boolean");
+
+// console.log(process.argv);
+
+let newPhrase = "";
+
+
+for (let i = 2; i< process.argv.length; i++){
+    newPhrase += process.argv[i] + " "
+}
+
+if (!newPhrase) {
+    console.log("Hello Boolean");
+} else {
+    console.log(newPhrase);
+}
